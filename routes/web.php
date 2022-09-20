54e232f8876d0563cc6b90a5a7e48ed676fc6c1a
mochiyuki0129 <mochi.yuki0129@gmail.com>
2022-09-20 16:55:51 +0900

    create

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
+
+Route::group(['middkeware' => 'can:view,folder'], function () {
+    Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
+    Route::get('folders/{folder}/tasks/{task}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{folder}/tasks/{task}/edit', [TaskController::class, 'edit']);
+});
+
+Route::post('/folders/create', [FolderController::class, 'create']);
+Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+Route::get('/auth/passwords/email', [HomeController::class, 'email'])->name('email');
+Route::post('/auth/passwords/email', [HomeController::class, 'email'])->name('email');
+Auth::routes();
